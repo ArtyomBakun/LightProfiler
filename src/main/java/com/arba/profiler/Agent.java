9dcf6bb1873ec64db96cfcb3dc64b0b014c7c62b
Artom Bacun <Artom_Bacun@epam.com>
2017-03-30 19:52:54 +0300

    Method timing

--- src/main/java/com/arba/profiler/Agent.java
+++ src/main/java/com/arba/profiler/Agent.java
@@ -1,5 +1,6 @@
 package com.arba.profiler;
 
+import java.io.FileOutputStream;
 import java.lang.instrument.Instrumentation;
 import java.io.IOException;
 
@@ -7,19 +8,7 @@ import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
-import org.apache.bcel.generic.ArrayType;
-import org.apache.bcel.generic.ClassGen;
-import org.apache.bcel.generic.ConstantPoolGen;
-import org.apache.bcel.generic.GETSTATIC;
-import org.apache.bcel.generic.INVOKEVIRTUAL;
-import org.apache.bcel.generic.Instruction;
-import org.apache.bcel.generic.InstructionHandle;
-import org.apache.bcel.generic.InstructionList;
-import org.apache.bcel.generic.LDC;
-import org.apache.bcel.generic.NEW;
-import org.apache.bcel.generic.MethodGen;
-import org.apache.bcel.generic.RETURN;
-import org.apache.bcel.generic.Type;
+import org.apache.bcel.generic.*;
 
 import static org.apache.bcel.Constants.*;
 
@@ -27,7 +16,6 @@ public class Agent
 {
     public static void premain(String agentArgs, Instrumentation instrumentation)
     {
-        System.out.println("Hello World! Java Agent");
 //        createSyntheticClass();
         modifyAppClass();
     }
@@ -36,18 +24,38 @@ public class Agent
         try
         {
             JavaClass appclass = Repository.lookupClass("com.arba.profiler.App");
-            printCode(appclass);
+//            modifyHelloWorld(appclass);
+            wrapMethod(appclass, "buildString");
+//            printClass(appclass);
         }
-        catch (ClassNotFoundException e)
+        catch (ClassNotFoundException | IOException e)
         {
             e.printStackTrace();
         }
     }
-    public static void printCode(JavaClass appclass) {
+
+    public static void printClass(JavaClass appclass) {
+        for (Method method : appclass.getMethods())
+        {
+            System.out.println(method.getName() + "\n" + method.getCode() + "\n****************\n\n");
+        }
+    }
+
+    private static void wrapMethod(JavaClass appclass, String methodName) throws IOException
+    {
+        for (Method method : appclass.getMethods())
+        {
+            if(method.getName().equals(methodName)){
+                addWrapper(new ClassGen(appclass), method);
+                break;
+            }
+        }
+    }
+    
+    public static void modifyHelloWorld(JavaClass appclass) throws TargetLostException, IOException
+    {
         ClassGen cg = new ClassGen(appclass);
         for (Method method : appclass.getMethods()) {
-//            System.out.println(method);
-            Code code = method.getCode();
             MethodGen mg = new MethodGen(method, cg.getClassName(), cg.getConstantPool());
             InstructionList il = mg.getInstructionList();
             for (InstructionHandle ih : il.getInstructionHandles())
@@ -55,59 +63,110 @@ public class Agent
                 Instruction i = ih.getInstruction();
                 if (i instanceof LDC)
                 {
-//                    System.out.println("\n#FFFFF#\n"+code+"\n#DDDDD#\n");
-                    try
-                    {
-                        LDC value = (LDC) i;
-//                        System.out.println("Found LDC instruction");
-//                        System.out.println(cg.getConstantPool());
-                        il.insert(value, new LDC(cg.getConstantPool().addString("Hello World! Modified App")));
-//                        System.out.println(cg.getConstantPool());
-                        il.delete(value);
-                        mg.setMaxLocals();
-                        mg.setMaxStack();
-                        mg.setInstructionList(il);
-                        cg.replaceMethod(method, mg.getMethod());
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                    }
-//                    System.out.println("\n#FFFFF#\n"+code+"\n#DDDDD#\n");
+                    LDC value = (LDC) i;
+                    il.insert(value, new LDC(cg.getConstantPool().addString("Hello World! Modified App")));
+                    il.delete(value);
+                    mg.setMaxLocals();
+                    mg.setMaxStack();
+                    mg.setInstructionList(il);
+                    cg.replaceMethod(method, mg.getMethod());
                 }
                 if (i instanceof NEW)
                 {
-                    //                    System.out.println("\n#FFFFF#\n"+code+"\n#DDDDD#\n");
-                    try
-                    {
-                        NEW value = (NEW) i;
-                        //                        System.out.println("Found LDC instruction");
-                        //                        System.out.println(cg.getConstantPool());
-                        if(value.getType(cg.getConstantPool()).getSignature().equals("Lcom/arba/profiler/A;"));
-//                        il.insert(value, new LDC(cg.getConstantPool().addString("Hello World! Modified App")));
-//                        //                        System.out.println(cg.getConstantPool());
-//                        il.delete(value);
-//                        mg.setMaxLocals();
-//                        mg.setMaxStack();
-//                        mg.setInstructionList(il);
-//                        cg.replaceMethod(method, mg.getMethod());
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                    }
-                    //                    System.out.println("\n#FFFFF#\n"+code+"\n#DDDDD#\n");
+                    NEW value = (NEW) i;
+                    if(value.getType(cg.getConstantPool()).getSignature().equals("Lcom/arba/profiler/A;"));
                 }
             }
-            try
-            {
-                cg.getJavaClass().dump("com/arba/profiler/App.class");
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
         }
+        appclass.dump("com/arba/profiler/App.class");
+    }
+
+    private static void addWrapper(ClassGen cgen, Method method) throws IOException
+    {
+        // set up the construction tools
+        InstructionFactory ifact = new InstructionFactory(cgen);
+        InstructionList ilist = new InstructionList();
+        ConstantPoolGen pgen = cgen.getConstantPool();
+        String cname = cgen.getClassName();
+        MethodGen wrapgen = new MethodGen(method, cname, pgen);
+        wrapgen.setInstructionList(ilist);
+
+        // rename a copy of the original method
+        MethodGen methgen = new MethodGen(method, cname, pgen);
+        cgen.removeMethod(method);
+        String iname = methgen.getName() + "$impl";
+        methgen.setName(iname);
+        cgen.addMethod(methgen.getMethod());
+        Type result = methgen.getReturnType();
+
+        // compute the size of the calling parameters
+        Type[] types = methgen.getArgumentTypes();
+        int slot = methgen.isStatic() ? 0 : 1;
+        for (int i = 0; i < types.length; i++) {
+            slot += types[i].getSize();
+        }
+
+        // save time prior to invocation
+        ilist.append(ifact.createInvoke("java.lang.System", "currentTimeMillis", Type.LONG, Type.NO_ARGS, INVOKESTATIC));
+        ilist.append(InstructionFactory.createStore(Type.LONG, slot));
+
+        // call the wrapped method
+        int offset = 0;
+        short invoke = INVOKESTATIC;
+        if (!methgen.isStatic()) {
+            ilist.append(InstructionFactory.createLoad(Type.OBJECT, 0));
+            offset = 1;
+            invoke = INVOKEVIRTUAL;
+        }
+        System.out.println("invoke=" + invoke);
+        for (int i = 0; i < types.length; i++) {
+            Type type = types[i];
+            ilist.append(InstructionFactory.createLoad(type, offset));
+            offset += type.getSize();
+        }
+        ilist.append(ifact.createInvoke(cname, iname, result, types, invoke));
+
+        // store result for return later
+        if (result != Type.VOID) {
+            ilist.append(InstructionFactory.createStore(result, slot+2));
+        }
+
+        // print time required for method call
+        ilist.append(ifact.createFieldAccess("java.lang.System", "out", 
+                new ObjectType("java.io.PrintStream"), GETSTATIC));
+        ilist.append(InstructionConstants.DUP);
+        ilist.append(InstructionConstants.DUP);
+        String text = "Call to method " + methgen.getName() + " took ";
+        ilist.append(new PUSH(pgen, text));
+        ilist.append(ifact.createInvoke("java.io.PrintStream", "print", 
+                Type.VOID, new Type[] { Type.STRING }, INVOKEVIRTUAL));
+        ilist.append(ifact.createInvoke("java.lang.System", "currentTimeMillis", 
+                Type.LONG, Type.NO_ARGS, INVOKESTATIC));
+        ilist.append(InstructionFactory.createLoad(Type.LONG, slot));
+        ilist.append(InstructionConstants.LSUB);
+        ilist.append(ifact.createInvoke("java.io.PrintStream", "print", 
+                Type.VOID, new Type[] { Type.LONG }, INVOKEVIRTUAL));
+        ilist.append(new PUSH(pgen, " ms."));
+        ilist.append(ifact.createInvoke("java.io.PrintStream", "println", 
+                Type.VOID, new Type[] { Type.STRING }, INVOKEVIRTUAL));
+
+        // return result from wrapped method call
+        if (result != Type.VOID) {
+            ilist.append(InstructionFactory.
+                    createLoad(result, slot+2));
+        }
+        ilist.append(InstructionFactory.createReturn(result));
+
+        System.out.println(wrapgen.getMethod().getName());
+        System.out.println(wrapgen.getMethod().getCode());
+
+        // finalize the constructed method
+        wrapgen.stripAttributes(true);
+        wrapgen.setMaxStack();
+        wrapgen.setMaxLocals();
+        cgen.addMethod(wrapgen.getMethod());
+        ilist.dispose();
+        cgen.getJavaClass().dump("com/arba/profiler/App.class");
     }
     
     private static void createSyntheticClass(){
--- src/main/java/com/arba/profiler/App.java
+++ src/main/java/com/arba/profiler/App.java
@@ -1,18 +1,35 @@
 package com.arba.profiler;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-/**
- * Created by artyom on 28.03.17.
- */
 public class App {
-    static final AtomicLong lookupAccountCalls = new AtomicLong();
+    
     public static void main( String[] args )
     {
-        lookupAccountCalls.incrementAndGet();
-        A a = new A();
-        System.out.println( "Hello World! App" );
+        String result = new App().buildString(120);
+        System.out.println("Constructed string of length " + result);
     }
- 
+
+    public String buildString(int length) {
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            result += (char)(i%26 + 'a');
+        }
+        return result;
+    }
+
+//    public String buildString$impl(int length) {
+//        String result = "";
+//        for (int i = 0; i < length; i++) {
+//            result += (char)(i%26 + 'a');
+//        }
+//        return result;
+//    }
+//
+//    public String buildString(int length) {
+//        long start = System.currentTimeMillis();
+//        String result = buildString$impl(length);
+//        System.out.println("Call to buildString$impl took " +
+//                (System.currentTimeMillis()-start) + " ms.");
+//        return result;
+//    }
+    
 }
-class A{}
